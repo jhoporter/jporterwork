5675ba2a250641ddd61f91c7e80600f9bfa145c5
Jho Porter <jhoanaporter@gmail.com>
2020-05-01 17:25:34 -0400

    deleted some unnecessary text

--- app/routes.py
+++ app/routes.py
@@ -10,17 +10,7 @@ import smtplib, ssl
 @app.route('/')
 @app.route('/index')
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home')
 
 
 @app.route('/samples')
