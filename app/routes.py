5af12342420f0460ab2ea563b3b5fb6fe4b47062
Jho Porter <jhoanaporter@gmail.com>
2020-05-07 12:01:08 -0400

    Contact form now working on index

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,22 @@ import smtplib, ssl
 @app.route('/index')
 def index():
     form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        article_type = form.article_type.data
+        word_count = form.word_count.data
+        message = form.message.data
+        subject = "Inquiry from " + name + "---" + email
+        text = "Type:" + " " + article_type + " \n" + "Word count: " + word_count + " \n" + "Message: " + message
+        send = 'Subject: {}\n\n{}'.format(subject, text)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        recipient = "jmporterwork@gmail.com"
+        server.starttls()
+        server.login("jmporterwork@gmail.com", "cy(5U((jj,r@RPMr%(CZ")
+        server.sendmail("jmporter@gmail.com", recipient, send)
+        flash('Message sent!')
+        return redirect(url_for('index'))
     return render_template('index.html', title='Home', form=form)
 
 
